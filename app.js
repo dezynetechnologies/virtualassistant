98cfae2a06bb591a79d0af947f5a56a19c04b209
Dheeraj Kaushik <dheeraj@dezyne.in>
2016-06-16 16:52:09 +0530

    Trying to auto-deploy

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === VALIDATION_TOKEN) {
     console.log("Validating webhook");
+    console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
     console.error("Failed validation. Make sure the validation tokens match.");
